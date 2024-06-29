aea467d12668fa049bc28b4af8b4e6df1176e656
Trevor Brandon-Harris <tbharris@ucdavis.edu>
2024-06-29 00:12:37 -0400

    added data

---
+++ tools/compress.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import sys
+import os
+import gzip
+
+for file_name in sys.argv[1:]:
+    file_name_no_ext = os.path.splitext(file_name)[0] # Get file name without extension
+    print("Reading " + file_name)
+    df = pd.read_csv(file_name, index_col="cell_specimen_id") # Read in csv file
+    print("Saving " + file_name)
+    df.to_hdf(file_name_no_ext + '.hdf5.lz4', complib="blosc:lz4", complevel=9, key=file_name_no_ext) # Output hdf file
